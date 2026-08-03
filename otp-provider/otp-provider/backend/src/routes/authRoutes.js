baabedba8b4ba5f8d63d38db98a3f3d085c519c1
ibrahemahmedabdo1-ai <ibrahemahmedabdo1@gmail.com>
2026-08-03 05:35:19 -0700

    Delete otp-provider/otp-provider/backend directory

--- otp-provider/otp-provider/backend/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { staffLogin, clientLogin } = require('../controllers/authController');
-
-router.post('/staff/login', staffLogin);
-router.post('/client/login', clientLogin);
-
-module.exports = router;
